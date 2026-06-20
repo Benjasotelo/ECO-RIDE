1f8a2db2e66694555430c328d3815b6b8b9819cb
benja <b.sotelo@duocuc.cl>
2026-06-20 19:06:06 -0400

    Se corrigio el pom, realizando un downgrade para implementar Swagger en MS-Usuario

--- ms-usuario/src/main/java/com/ecoride/ms_usuario/dto/UsuarioResponseDTO.java
+++ ms-usuario/src/main/java/com/ecoride/ms_usuario/dto/UsuarioResponseDTO.java
@@ -1,18 +1,30 @@
 package com.ecoride.ms_usuario.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Respuesta con datos del usuario")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UsuarioResponseDTO {
+
+    @Schema(description = "ID del usuario", example = "1")
     private Long id;
+
+    @Schema(description = "Nombre de usuario", example = "victor_admin")
     private String nombreUsuario;
+
+    @Schema(description = "Correo electrónico", example = "victor@ecoride.cl")
     private String correoElectronico;
+
+    @Schema(description = "Rol del usuario", example = "ADMIN")
     private String rol;
+
+    @Schema(description = "Estado activo del usuario", example = "true")
     private Boolean activo;
 }
