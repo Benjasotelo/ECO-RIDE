eff3308ce315d2222cd19b911371b1b85f4c27e4
Victor Loaiza <vi.loaiza@duocuc.cl>
2026-06-20 20:19:36 -0400

    agregar eureka y API gateway

---
+++ eureka-server/src/main/java/com/ecoride/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecoride.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // <-- Esta anotación es la magia que levanta el panel de control
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
--- ms-calificaciones/src/main/java/com/ecoride/ms_calificaciones/MsCalificacionesApplication.java
+++ ms-calificaciones/src/main/java/com/ecoride/ms_calificaciones/MsCalificacionesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsCalificacionesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsCalificacionesApplication.class, args);
 	}
-
 }
